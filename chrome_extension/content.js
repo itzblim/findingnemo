328cfeffaaeda5d39a5d63043c20b7fcb5015a72
Bryanwoo <bubu.woo7999@gmail.com>
2021-06-19 18:15:40 +0800

    Trial for popup
    html and css code trial
    some additional photos added in

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -3,9 +3,9 @@ console.log("Check if content.js is running");
 function replaceImages() {
     let images = document.querySelectorAll('img');
 
-    for (var i = 0; i < img_array.length; i++) {
+    for (var i = 0; i < imgArray.length; i++) {
         for (elt of images) {
-            if (elt.src == img_array[i]) {
+            if (elt.src == imgArray[i]) {
                 let file = 'images/cat.jpg';
                 let url = chrome.extension.getURL(file);
                 elt.src = url;
@@ -16,7 +16,9 @@ function replaceImages() {
 
 //To find a slightly more permanent fix than this. 
 //Somehow functions which wait for webpage to load doesnt fully wait for it to load
-setTimeout(() => { replaceImages(); }, 5000)
+
+//this function must load only after url finder has finish loading 
+setTimeout(() => { replaceImages(); }, 10000)
 
 
 
