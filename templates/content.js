0fb0be110476786b2eea68cc27134eda612f8805
Bryanwoo <bubu.woo7999@gmail.com>
2021-06-27 01:33:59 +0800

    div id tags imgs

--- templates/content.js
+++ templates/content.js
@@ -1,4 +1,5 @@
 console.log("Check if content.js is running");
+var localImgArray = ['images/cat.jpg', 'images/cat2.jpg', 'images/cat3.jpg']
 
 function replaceImages() {
     let images = document.querySelectorAll('img');
@@ -6,19 +7,21 @@ function replaceImages() {
     for (var i = 0; i < imgArray.length; i++) {
         for (elt of images) {
             if (elt.src == imgArray[i]) {
-                let file = 'images/cat.jpg';
+                let file = localImgArray[i];
                 let url = chrome.extension.getURL(file);
                 elt.src = url;
+                elt.setAttribute("id", `Pic${i}`)
             }
         }
     }
 }
 
+
 //To find a slightly more permanent fix than this. 
 //Somehow functions which wait for webpage to load doesnt fully wait for it to load
 
 //this function must load only after url finder has finish loading 
-setTimeout(() => { replaceImages(); }, 10000)
+setTimeout(() => { replaceImages(); }, 5000)
 
 
 
