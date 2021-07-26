caa4dbb06696f9e975dbb77fcae73756a6e44aff
itzblim <54736906+itzblim@users.noreply.github.com>
2021-07-26 21:13:47 +0800

    Add files via upload
    Resolved issues with run/pause. Integrated searching and loading pages with run/pause settings.

--- templates/popup.js
+++ templates/popup.js
@@ -5,8 +5,6 @@ var counterPosition = 1;
 var newImgArray;
 var oldImgArray;
 var currentUrl;
-//If true, then the image search is activated--> To be done by the run and pause button
-var imageSearch = false;
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("searchBar").addEventListener("keypress", searchBar);
@@ -14,70 +12,74 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("arrowUp").addEventListener("click", upArrow);
   document.getElementById("arrowDown").addEventListener("click", downArrow);
   document.getElementById("escape").addEventListener("click", escape);
-  document.getElementById("runButton").addEventListener("click", showRunAlert);
+  document.getElementById("runButton").addEventListener("click", runAlert);
+  document.getElementById("pauseButton").addEventListener("click", pauseAlert);
   document.getElementById("textHits").innerHTML = textHits;
   document.getElementById("imgHits").innerHTML = imgHits;
-  showRunAlert();
+  chrome.storage.local.get(["picSearchStatus"], (data) => {
+    chrome.storage.local.set(
+      {
+        picSearchStatus: data.picSearchStatus === undefined
+          ? "PIC-Search Status: Run"
+          : data.picSearchStatus
+      }
+    );
+    document.getElementById("picSearchStatus").innerHTML =
+      data.picSearchStatus === undefined
+        ? "PIC-Search Status: Run"
+        : data.picSearchStatus;
+  });
+  startRun();
 });
 
 function searchBar(event) {
   counterPosition = 1;
   searchElem = document.getElementById("searchBar").value;
   if (event.key === "Enter") {
-    if (imageSearch) {
-      $.ajax({
-        type: "POST",
-        contentType: "application/json;charset=utf-8",
-        url: "http://localhost:5000/search-link/",
-        traditional: "true",
-        data: JSON.stringify({ searchElem }),
-        dataType: "json",
-        complete: function () {
-          $.ajax({
-            type: "GET",
-            contentType: "application/json;charset=utf-8",
-            url: "http://localhost:5000/getImgUrl-link/",
-            dataType: "json",
-            success: function (res) {
-              var n = res.length / 2;
-              newImgArray = res
-                .slice(0, n - 1)
-                .map((x) =>
-                  "http://localhost:8000/templates/images/new/".concat(x)
-                );
-              chrome.tabs.query(
-                { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-                function (tabs) {
-                  chrome.tabs.sendMessage(
-                    tabs[0].id,
-                    {
-                      from: "popup",
-                      subject: "imglists",
-                      oldImgs: newImgArray,
-                      newImgs: newImgArray,
-                    },
-                    function (res) {}
+    chrome.storage.local.get(["picSearchStatus"], (data) => {
+      if (data.picSearchStatus === "PIC-Search Status: Run") {
+        $.ajax({
+          type: "POST",
+          contentType: "application/json;charset=utf-8",
+          url: "http://localhost:5000/search-link/",
+          traditional: "true",
+          data: JSON.stringify({ searchElem }),
+          dataType: "json",
+          complete: function () {
+            $.ajax({
+              type: "GET",
+              contentType: "application/json;charset=utf-8",
+              url: "http://localhost:5000/getImgUrl-link/",
+              dataType: "json",
+              success: function (res) {
+                var n = res.length / 2;
+                newImgArray = res
+                  .slice(0, n - 1)
+                  .map((x) =>
+                    "http://localhost:8000/templates/images/new/".concat(x)
                   );
-                  upArrow();
-                }
-              );
-            },
-          });
-        },
-      });
-    }
-    //word search done here
-    if (searchElem) {
-      chrome.tabs.query(
-        {
-          active: true,
-          lastFocusedWindow: true,
-        },
-        function (tabs) {
-          chrome.tabs.executeScript(tabs[0].id, { file: "searchText.js" });
-        }
-      );
-    }
+                chrome.tabs.query(
+                  { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+                  function (tabs) {
+                    chrome.tabs.sendMessage(
+                      tabs[0].id,
+                      {
+                        from: "popup",
+                        subject: "imglists",
+                        oldImgs: newImgArray,
+                        newImgs: newImgArray,
+                      },
+                      function (res) {}
+                    );
+                    upArrow();
+                  }
+                );
+              },
+            });
+          },
+        });
+      }
+    });
   }
 }
 
@@ -144,46 +146,82 @@ function escape() {
   window.close();
 }
 
-function showRunAlert() {
-  chrome.tabs.query(
-    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
-    function (tabs) {
-      currentUrl = tabs[0].url.split("#")[0];
-      $.ajax({
-        type: "POST",
-        contentType: "application/json;charset=utf-8",
-        url: "http://localhost:5000/postCurrentUrl-link/",
-        traditional: "true",
-        data: JSON.stringify({ currentUrl }),
-        dataType: "json",
-        complete: function (data) {
+function startRun() {
+  chrome.storage.local.get(["picSearchStatus"], (data) => {
+    if (data.picSearchStatus == "PIC-Search Status: Run") {
+      chrome.tabs.query(
+        { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+        function (tabs) {
+          currentUrl = tabs[0].url.split("#")[0];
           $.ajax({
-            type: "GET",
+            type: "POST",
             contentType: "application/json;charset=utf-8",
-            url: "http://localhost:5000/getImgUrl-link/",
+            url: "http://localhost:5000/postCurrentUrl-link/",
+            traditional: "true",
+            data: JSON.stringify({ currentUrl }),
             dataType: "json",
-            success: function (res) {
-              var n = res.length / 2;
-              newImgArray = res
-                .slice(0, n - 1)
-                .map((x) =>
-                  "http://localhost:8000/templates/images/new/".concat(x)
-                );
-              oldImgArray = res.slice(n, res.length - 1);
-              chrome.tabs.sendMessage(
-                tabs[0].id,
-                {
-                  from: "popup",
-                  subject: "imglists",
-                  oldImgs: oldImgArray,
-                  newImgs: newImgArray,
+            complete: function (data) {
+              $.ajax({
+                type: "GET",
+                contentType: "application/json;charset=utf-8",
+                url: "http://localhost:5000/getImgUrl-link/",
+                dataType: "json",
+                success: function (res) {
+                  var n = res.length / 2;
+                  newImgArray = res
+                    .slice(0, n - 1)
+                    .map((x) =>
+                      "http://localhost:8000/templates/images/new/".concat(x)
+                    );
+                  oldImgArray = res.slice(n, res.length - 1);
+                  chrome.tabs.sendMessage(
+                    tabs[0].id,
+                    {
+                      from: "popup",
+                      subject: "imglists",
+                      oldImgs: oldImgArray,
+                      newImgs: newImgArray,
+                    },
+                    function (res) {}
+                  );
                 },
-                function (res) {}
-              );
+              });
             },
           });
-        },
-      });
+        }
+      );
     }
-  );
+  });
+}
+
+function runAlert() {
+  chrome.storage.local.set({ picSearchStatus: "PIC-Search Status: Run" });
+  chrome.storage.local.get(["picSearchStatus"], (data) => {
+    document.getElementById("picSearchStatus").innerHTML = data.picSearchStatus;
+    chrome.tabs.query(
+      { active: true, windowType: "normal", currentWindow: true },
+      function (d) {
+        var tabId = d[0].id;
+        chrome.browserAction.setIcon({
+          path: "/images/Logo_19px_green.png",
+        });
+      }
+    );
+  });
+}
+
+function pauseAlert() {
+  chrome.storage.local.set({ picSearchStatus: "PIC-Search Status: Pause" });
+  chrome.storage.local.get(["picSearchStatus"], (data) => {
+    document.getElementById("picSearchStatus").innerHTML = data.picSearchStatus;
+    chrome.tabs.query(
+      { active: true, windowType: "normal", currentWindow: true },
+      function (d) {
+        var tabId = d[0].id;
+        chrome.browserAction.setIcon({
+          path: "/images/Logo_19px_red.png",
+        });
+      }
+    );
+  });
 }
